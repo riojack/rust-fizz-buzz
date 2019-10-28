da27f49d95fcf6b0ded95f46085bead7d139c368
riojack <ayjay122@gmail.com>
2019-10-28 14:26:23 -0500

    'fizz' when number is three

--- src/fizzbuzz/fizz.rs
+++ src/fizzbuzz/fizz.rs
@@ -1,3 +1,13 @@
-pub fn fizzbuzz() -> String {
-    String::from("")
+pub fn fizzbuzz(num: usize) -> String {
+    String::from("fizz")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::fizzbuzz::fizz::fizzbuzz;
+
+    #[test]
+    fn withThreeReturnsFizz() {
+        assert_eq!(fizzbuzz(3), "fizz");
+    }
 }
